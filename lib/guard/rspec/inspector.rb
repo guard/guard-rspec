eca456edb038c67d76bcdf4995bb80a72a83a6f3
Rémy Coutable <rymai@rymai.com>
2011-03-01 00:14:27 +0100

    Improved Inspector class a bit

--- lib/guard/rspec/inspector.rb
+++ lib/guard/rspec/inspector.rb
@@ -2,39 +2,39 @@ module Guard
   class RSpec
     module Inspector
       class << self
-        
+
         def clean(paths)
           paths.uniq!
           paths.compact!
-          paths = paths.select { |p| spec_file?(p) || spec_folder?(p) }
-          paths = paths.delete_if { |p| included_in_other_path?(p, paths) } 
-          clear_spec_files_list
-          paths
+          clear_spec_files_list_after do
+            paths = paths.select { |p| spec_file?(p) || spec_folder?(p) }
+          end
+          paths.reject { |p| included_in_other_path?(p, paths) }
         end
-        
+
       private
-        
-        def spec_folder?(path)
-          path.match(/^\/?spec/) && !path.match(/\..+$/)
-        end
-        
+
         def spec_file?(path)
           spec_files.include?(path)
         end
-        
-        def spec_files
-          @spec_files ||= Dir.glob("spec/**/*_spec.rb")
+
+        def spec_folder?(path)
+          path.match(%r{^spec[^\.]*$})
         end
-        
-        def clear_spec_files_list
+
+        def spec_files
+          @spec_files ||= Dir["spec/**/*_spec.rb"]
+        end
+
+        def clear_spec_files_list_after
+          yield
           @spec_files = nil
         end
-        
+
         def included_in_other_path?(path, paths)
-          paths = paths.select { |p| p != path }
-          paths.any? { |p| path.include?(p) && (path.gsub(p, '')).include?("/") }
+          (paths - [path]).any? { |p| path.include?(p) && path.sub(p, '').include?('/') }
         end
-        
+
       end
     end
   end
