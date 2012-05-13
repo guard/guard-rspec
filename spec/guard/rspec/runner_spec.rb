guard/guard-rspec

commit c3b5c658ef28b4feb41136c2ae9a9893efda58fc

Formatters from .rspec file, guard/guard-rspec#98
Conflicts:

	lib/guard/rspec/runner.rb

lib/guard/rspec/runner.rb
@@ -62,12 +62,22 @@ module Guard
                          end
       end
 
+      def default_formatter
+        @default_formatter ||= unless @options[:cli] =~ formatter_regex
+          file_name = "#{Dir.pwd}/.rspec"
+          if File.exist?(file_name)
+            s = File.read(file_name)
+            s.scan(formatter_regex).flatten.map{|i| "-f #{i}"}.join(' ')
+          end
+        end || '-f progress'
+      end
+
     private
 
       def rspec_arguments(paths, options)
         arg_parts = []
         arg_parts << options[:cli]
-        arg_parts << "-f progress" if !options[:cli] || options[:cli].split(/[\s=]/).none? { |w| %w[-f --format].include?(w) }
+        arg_parts << default_formatter unless options[:cli] =~ formatter_regex
         if @options[:notification]
           arg_parts << "-r #{File.dirname(__FILE__)}/formatters/notification_#{rspec_class.downcase}.rb"
           arg_parts << "-f Guard::RSpec::Formatter::Notification#{rspec_class}#{rspec_version == 1 ? ":" : " --out "}/dev/null"
@@ -84,7 +94,6 @@ module Guard
         cmd_parts << "bundle exec" if bundler?
         cmd_parts << rspec_executable
         cmd_parts << rspec_arguments(paths, options)
-
         cmd_parts.compact.join(' ')
       end
 
@@ -197,6 +206,10 @@ module Guard
             UI.info %{DEPRECATION WARNING: The :#{key} option is deprecated. Pass standard command line argument "--#{value}" to RSpec with the :cli option.}
           end
         end
+      end
+
+      def formatter_regex
+        @formatter_regex ||= /(?:^|\s)(?:-f\s*|--format(?:=|\s+))(\w+)/
       end
 
     end

spec/guard/rspec/runner_spec.rb
@@ -213,7 +213,33 @@ describe Guard::RSpec::Runner do
             end
           end
 
-          context ":cli => '-f doc'" do
+          context ":cli => '-fdoc'" do
+            subject { described_class.new(:cli => '-fdoc') }
+
+            it 'respects formatter passed in CLI options to RSpec' do
+              subject.should_receive(:system).with(
+                "bundle exec rspec -fdoc -r #{@lib_path.join('guard/rspec/formatters/notification_rspec.rb')} " <<
+                '-f Guard::RSpec::Formatter::NotificationRSpec --out /dev/null --failure-exit-code 2 spec'
+              ).and_return(true)
+
+              subject.run(['spec'])
+            end
+          end
+
+          context ":cli => '--format doc'" do
+            subject { described_class.new(:cli => '--format doc') }
+
+            it 'respects formatter passed in CLI options to RSpec' do
+              subject.should_receive(:system).with(
+                "bundle exec rspec --format doc -r #{@lib_path.join('guard/rspec/formatters/notification_rspec.rb')} " <<
+                '-f Guard::RSpec::Formatter::NotificationRSpec --out /dev/null --failure-exit-code 2 spec'
+              ).and_return(true)
+
+              subject.run(['spec'])
+            end
+          end
+
+          context ":cli => '--format=doc'" do
             subject { described_class.new(:cli => '--format=doc') }
 
             it 'respects formatter passed in CLI options to RSpec' do
@@ -356,4 +382,16 @@ describe Guard::RSpec::Runner do
     end
   end
 
+  describe '#default_formatter' do
+    before { Dir.stub(:pwd).and_return(@fixture_path) }
+    it 'returns formatter from .rspec file' do
+      subject.default_formatter.should == '-f documentation'
+    end
+    context 'if .rspec file no exists' do
+      before { File.stub(:exist?).and_return(false) }
+      it 'returns progress formatter' do
+        subject.default_formatter == '-f progress'
+      end
+    end
+  end
 end

spec/fixtures/.rspec
@@ -1,0 +1,2 @@
+--colour
+--format documentation

commit ce13014c70a1f05620fa1a9668bcc979004d95ea

Fix a RSpec formatter issue
Commit c3b5c658ef28b4feb41136c2ae9a9893efda58fc introduced parsing of
the .rspec file if present with a fallback to a default formatter but
didn't handle the case where a .rspec file exists but without any
formatter option in it. In that case, no formatter was used meaning
that users got no feedback of the tests rsesults.

This commit fixes this issue by using a default formatter if there is
no .rspec file and no `--format` option given through the `:cli` option
or if there is a .rspec file but without any `--format` option in it.

lib/guard/rspec/runner.rb
@@ -62,14 +62,16 @@ module Guard
                          end
       end
 
-      def default_formatter
-        @default_formatter ||= unless @options[:cli] =~ formatter_regex
+      def parsed_or_default_formatter
+        @parsed_or_default_formatter ||= begin
           file_name = "#{Dir.pwd}/.rspec"
-          if File.exist?(file_name)
-            s = File.read(file_name)
-            s.scan(formatter_regex).flatten.map{|i| "-f #{i}"}.join(' ')
+          parsed_formatter = if File.exist?(file_name)
+            formatters = File.read(file_name).scan(formatter_regex).flatten
+            formatters.map { |formatter| "-f #{formatter}" }.join(' ')
           end
-        end || '-f progress'
+
+          parsed_formatter.nil? || parsed_formatter.empty? ? '-f progress' : parsed_formatter
+        end
       end
 
     private
@@ -77,8 +79,8 @@ module Guard
       def rspec_arguments(paths, options)
         arg_parts = []
         arg_parts << options[:cli]
+        arg_parts << parsed_or_default_formatter unless options[:cli] =~ formatter_regex
         if @options[:notification]
-          arg_parts << default_formatter unless options[:cli] =~ formatter_regex
           arg_parts << "-r #{File.dirname(__FILE__)}/formatters/notification_#{rspec_class.downcase}.rb"
           arg_parts << "-f Guard::RSpec::Formatter::Notification#{rspec_class}#{rspec_version == 1 ? ":" : " --out "}/dev/null"
         end

spec/guard/rspec/runner_spec.rb
@@ -302,7 +302,7 @@ describe Guard::RSpec::Runner do
 
             it 'runs without notification formatter' do
               subject.should_receive(:system).with(
-                'bundle exec rspec --failure-exit-code 2 spec'
+                'bundle exec rspec -f progress --failure-exit-code 2 spec'
               ).and_return(true)
 
               subject.run(['spec'])
@@ -382,16 +382,40 @@ describe Guard::RSpec::Runner do
     end
   end
 
-  describe '#default_formatter' do
-    before { Dir.stub(:pwd).and_return(@fixture_path) }
-    it 'returns formatter from .rspec file' do
-      subject.default_formatter.should eq '-f documentation'
+  describe '#parsed_or_default_formatter' do
+    context '.rspec file exists' do
+      before do
+        Dir.stub(:pwd).and_return(@fixture_path)
+      end
+
+      context 'and includes a --format option' do
+        before do
+          File.stub(:read).and_return("--colour\n--format documentation")
+        end
+
+        it 'returns the formatter from .rspec file' do
+          subject.parsed_or_default_formatter.should eq '-f documentation'
+        end
+      end
+
+      context 'but doesn\'t include a --format option' do
+        before do
+          File.stub(:read).and_return("--colour")
+        end
+
+        it 'returns progress formatter' do
+          subject.parsed_or_default_formatter.should eq '-f progress'
+        end
+      end
     end
-    context 'if .rspec file no exists' do
+
+    context '.rspec file doesn\'t exists' do
       before { File.stub(:exist?).and_return(false) }
+
       it 'returns progress formatter' do
-        subject.default_formatter.should eq '-f progress'
+        subject.parsed_or_default_formatter.should eq '-f progress'
       end
     end
   end
+
 end