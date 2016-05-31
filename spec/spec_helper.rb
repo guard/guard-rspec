f565af68f9d9fd29e24d0f34a297466d99db2ef4
Cezary Baginski <cezary@chronomantic.net>
2016-05-31 08:04:36 +0200

    stop using Guard::UI in formatter + isolated test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,6 +29,13 @@ end
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.register_ordering :global do |examples|
+    examples.partition { |ex| ex.metadata[:type] != :acceptance }.flatten(1)
+  end
+
+  # Use global for running acceptance tests last
+  config.order = :global
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
@@ -88,12 +95,6 @@ RSpec.configure do |config|
   # end of the spec run, to help surface which specs are running
   # particularly slow.
   # config.profile_examples = 10
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = :random
 
   # Seed global randomization in this process using the `--seed` CLI option.
   # Setting this allows you to use `--seed` to deterministically reproduce
